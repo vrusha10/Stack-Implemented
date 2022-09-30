41f85f9bd8d0bb243b4ae46df137569e86fa9aa0
vrushabh <vrushabh1452@gmail.com>
2022-09-30 18:04:50 +0530

    First Commit - Stack Impemented

---
+++ Stackimplemented.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+#define n 100
+class Stack
+{
+   int *arr;
+   int top;
+   
+   public:
+   Stack()
+   {
+        arr=new int[n];
+        top=-1;
+   }
+    
+   void push(int x)
+   {
+        if(top==n-1)
+        {
+            cout<<"Stack Overflow"<<endl;
+         }
+       top++;
+       arr[top]=x;
+    }
+
+    void pop()
+    {
+       if(top==-1)
+       {
+           cout<<"No element"<<endl;
+       }
+       top--;
+     }
+
+     int Top()
+     {
+        if(top==-1)
+        {
+            cout<<"No element in stack"<<endl;
+            return -1;
+         }
+         return arr[top];
+      }
+
+      bool empty()
+      {
+          return top==-1;
+       }
+};
+int main()
+{
+   Stack st;
+   st.push(1);
+   st.push(2);
+   st.push(3);
+   cout<<st.Top()<<endl;
+   st.pop();
+   cout<<st.Top()<<endl;
+   st.pop();
+   st.pop();
+   st.pop();
+   cout<<st.empty()<<endl;
+}
